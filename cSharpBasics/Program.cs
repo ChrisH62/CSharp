97742d12862179846854ed398f953435d67e2427
ChrisH62 <88059992+ChrisH62@users.noreply.github.com>
2022-06-09 12:25:01 -0400

    a

---
+++ cSharpBasics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cSharpBasics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
